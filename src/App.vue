62db5cbaa32fd0cb2f178969fb66d72bd5d053c5
Mo Ghanbari <mostafa.qanbary@gmail.com>
2020-04-14 18:08:52 -0400

    First ui. a template from codepen.io

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">About</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import 'https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css';
+</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-form">
+    <p>This is login form</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LoginForm',
+  props: {
+    msg: String
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
